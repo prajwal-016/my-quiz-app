7c5b80a86da6b98495b055002894d4403a00ef6d
prajwal-016 <prajwalprajju705@gmail.com>
2026-05-26 00:02:45 +0530

    fix: explicit base path pathing

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/my-quiz-app/", // ⚠️ Double-check that it matches your repo name exactly
-});
+  base: '/my-quiz-app/', 
+})
